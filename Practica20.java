ef7aa3a75aa150716f1595ca3f496e6ef537d183
AlexisJavier1234 <143725720+AlexisJavier1234@users.noreply.github.com>
2023-09-07 09:52:30 -0500

    Add files via upload
    Practica 2.0 nombre y area de rectangulo

---
+++ Practica20.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2.pkg0;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int Base=10;
+        int altura=3;
+        int areaRectangulo = Base*altura;
+        System.out.println("El area del rectangulo es:"+areaRectangulo);
+        String nombre="ALEXIS";
+        System.out.println("el nombre es."+nombre);
+    }
+    
+}
